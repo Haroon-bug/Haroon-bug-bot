6b9269a089b36260d434089c25a936db21b06635
THE PROGRAMMER <142110420+Itxxwasi@users.noreply.github.com>
2024-08-30 15:35:49 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -69,7 +69,7 @@ global.mess = {
     error: 'Error!',
 }
 
-global.thumb = fs.readFileSync('./XeonMedia/thumb.jpg')
+global.thumb = fs.readFileSync('./xdharoon/thumb.jpg')
 
 let file = require.resolve(__filename)
 fs.watchFile(file, () => {
